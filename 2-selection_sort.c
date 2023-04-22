3f8bedf366ee2a1de71c26411636f5a76ba4fce8
RIvAL-411 <cliffomanyo@gmail.com>
2023-04-22 04:42:03 -0700

    selection sort correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,12 +8,13 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	int i, j, n = size, min, temp;
+	int min, temp;
+	size_t i, j;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; j < n; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
